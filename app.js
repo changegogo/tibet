b4c172857b4662b8a12919ac2446f2a9bf7107ab
dailliwang <758017188@qq.com>
2018-06-06 13:31:44 +0800

    npm start test

--- app.js
+++ app.js
@@ -22,5 +22,27 @@ module.exports = app => {
 
             await app.model.sync({force: false});
         });
+    }else {
+        app.beforeStart(async function () {
+            // 关联关系
+            app.model.Film.belongsTo( app.model.Filmtype  , {"foreignKey":"filmtype_id"} );
+            app.model.Filmtype.hasMany(     app.model.Film , {"foreignKey":"filmtype_id"} );
+
+            app.model.Novel.belongsTo( app.model.Noveltype  , {"foreignKey":"noveltype_id"} );
+            app.model.Noveltype.hasMany(     app.model.Novel , {"foreignKey":"noveltype_id"} );
+
+            app.model.Game.belongsTo( app.model.Gametype  , {"foreignKey":"gametype_id"} );
+            app.model.Gametype.hasMany(     app.model.Game , {"foreignKey":"gametype_id"} );
+
+            app.model.Mynovel.belongsTo( app.model.Novel  , {"foreignKey":"nove_id"} );
+            app.model.Novel.hasMany(     app.model.Mynovel , {"foreignKey":"nove_id"} );
+
+            app.model.Myfilm.belongsTo( app.model.Film  , {"foreignKey":"film_id"} );
+            app.model.Film.hasMany(     app.model.Myfilm , {"foreignKey":"film_id"} );
+
+            app.model.Mywifi.belongsTo( app.model.Wifi  , {"foreignKey":"wifi_id"} );
+            app.model.Wifi.hasMany(     app.model.Mywifi , {"foreignKey":"wifi_id"} );
+            await app.model.sync({force: false});
+        });
     }
 };
