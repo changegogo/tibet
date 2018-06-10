7104224b477b9f19c04e8b272dc2f2cfe1eabe9e
dailliwang <758017188@qq.com>
2018-06-10 18:39:01 +0800

    fix movietype.nj

--- app/controller/wxSell.js
+++ app/controller/wxSell.js
@@ -7,9 +7,11 @@ class wxSellController extends Controller {
         let { body, total_fee } = ctx.request.body;
         let attach = "23131256231";
         let mch_id = '111111'; // 商户id
-        let openid = '11111';
+        let openid = '11111'; // 开放平台id
         let bookingNo = commonUtils.generateTradeNumber(); // 订单号
         let notify_url = 'http://localhost/wx/notify'; // 回调地址
+        // 将订单插入自己数据库 todo
+        
         wxPay.order(attach, body, mch_id, openid, bookingNo, total_fee, notify_url)
             .then((data) => {
                 ctx.render('home.wxpay', {args: data});
