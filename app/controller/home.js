16011e6f69fd9e2c1f9114f6f1fd0284696a416d
dailliwang <758017188@qq.com>
2018-09-04 18:25:00 +0800

    a

--- app/controller/home.js
+++ app/controller/home.js
@@ -591,7 +591,7 @@ module.exports = app => {
             ctx.body = {
                 name: '春雨医生',
                 img: '/public/upload/chunyu_v.png',
-                httpurl: 'http://api.chunyuyisheng.com/download/chunyu/latest/?vendor=tianluqidong'
+                httpurl: 'http://api.chunyuyisheng.com/download/chunyu/latest/?vendor=tianluqidong&ylkjtag=ylkjadv'
             };
         }
         
@@ -599,7 +599,7 @@ module.exports = app => {
             ctx.body = {
                 name: '春雨医生',
                 img: '/public/upload/chunyu.png',
-                httpurl: 'http://api.chunyuyisheng.com/download/chunyu/latest/?vendor=tianlushouye'
+                httpurl: 'http://api.chunyuyisheng.com/download/chunyu/latest/?vendor=tianlushouye&ylkjtag=ylkjadv'
             };
         }
 
